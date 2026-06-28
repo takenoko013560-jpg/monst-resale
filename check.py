52c32a150d57be70441ae5f54000d611c597078a
takenoko013560-jpg <takenoko013560@gmail.com>
2026-06-28 12:26:51 +0900

    Update check.py

--- check.py
+++ check.py
@@ -17,7 +17,7 @@ async def main():
         browser = await p.chromium.launch(headless=True)
         page = await browser.new_page(locale="ja-JP")
         await page.goto(URL, wait_until="domcontentloaded", timeout=60000)
-        await page.wait_for_timeout(3000)
+        await page.wait_for_timeout(1000)
 
         text = await page.locator("body").inner_text()
         await browser.close()
